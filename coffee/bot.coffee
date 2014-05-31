91ba75a48a8b99c55df119278eebcf22be1eeb6d
Greg Fleming <gfleming@ctmtg.com>
2014-05-31 18:26:34 -0400

    [master] more hacking at the top level algorithm.  it runs now, though it is pretty dumb.  best score around 1k

--- coffee/bot.coffee
+++ coffee/bot.coffee
@@ -2,26 +2,47 @@
 class Bot
   constructor: (@grid) ->
     console.log @grid
+    @boardState = new BoardStateFactory().fromGrid(@grid)
 
-  # this is pretty much pseudocode right now...
+  evaluate: (boardState) ->
+    boardState.emptySquares().length
+
   expectimax: (boardState, depth, ourTurn) ->
     self = this
     if depth == 0
       return this.evaluate boardState
 
+    if ourTurn == false and boardState.emptySquares().length == 0
+      return 0  # we lose
+
     if ourTurn
-      childScores = boardState.legalMoves().map (move) ->
+      childScores = boardState.legalMoves().map (move) =>
         this.expectimax boardState.move(move), depth-1, false
       return Math.max childScores...
     else
       emptySquares = boardState.emptySquares()
-      return emptySquares.map (square) ->
-        [ [2, 0.9], [4, 0.1] ].map((a) ->
-          v,p = a...
-          p * self.expectimax boardState.populateSquare(square, v), depth-1, true
-        ).flatten.average
+      x = emptySquares.map (square) ->
+        [ [2, 0.9], [4, 0.1] ].map (a) ->
+          [v,p] = a
+          p * self.expectimax boardState.populateSquare(square, v), depth, true
+      x = _.flatten x
+      sum = _.reduce x, (a,b) -> a+b
+      sum / x.length
 
-  getBestMove: ->
-    '1'
+  getBestMove: =>
+    # pick the best legal move.  A move's score is calculated by the expectimax value 
+    # of having moved the board that way.  
+    moveScores = @boardState.legalMoves().map (move) =>
+      { move: move, score: this.expectimax(@boardState.move(move), 1, false) }
+    console.log moveScores
+    sortedScores = _.sortBy moveScores, (ms) -> ms.score
+    this.translateMove sortedScores[0].move
 
+  translateMove: (move) ->
+    moves =
+      up: '0'
+      right: '1'
+      down: '2'
+      left: '3'
+    moves[move]
 
--- js/ai.js
+++ js/ai.js
@@ -2,93 +2,10 @@ function AI(grid) {
   this.grid = grid;
 }
 
-// TODO:  columns and rows collections
-
-AI.prototype.canGoUp = function() {
-  // needs either an empty space at the top of the column, 
-  //console.log(this.grid);
-  for(var x=0; x<4; x++) {
-    var spaceFound = false;
-    for(var y=0; y<4; y++) {
-      var tile = this.grid.cells[x][y];
-      if (tile) {
-        if(spaceFound) {
-          // here we found a column that has a space 
-          // above a non-space; we can go up. 
-          return true;
-        }
-      } else {
-        spaceFound = true;
-      }
-    }
-  }
-  
-  // or a pair of the same number in the same col with nothing between
-  for(var x=0; x<4; x++) {
-    var currentNum = null;
-    for(var y=0; y<4; y++) {
-      var tile = this.grid.cells[x][y];
-      if (tile) {
-        if (tile.value == currentNum) {
-          // this tile's value is the same as that of its nearest upward
-          // neighbor; we can go up. 
-          return true;
-        } else {
-          currentNum = tile.value;
-        }
-      }
-    }
-  }
-
-  return false;
-}
-
-AI.prototype.canGoLeft = function() {
-  // needs either a space to the left of a non-space,
-  for(var y=0; y<4; y++) {
-    var spaceFound = false;
-    for(var x=0; x<4; x++) {
-      var tile = this.grid.cells[x][y];
-      if (tile) {
-        if(spaceFound) {
-          return true;
-        }
-      } else {
-        spaceFound = true;
-      }
-    }
-  }
-  
-  // or a pair of the same number in the same row with nothing between
-  for(var y=0; y<4; y++) {
-    var currentNum = null;
-    for(var x=0; x<4; x++) {
-      var tile = this.grid.cells[x][y];
-      if (tile) {
-        if (tile.value == currentNum) {
-          return true;
-        } else {
-          currentNum = tile.value;
-        }
-      }
-    }
-  }
-
-  return false;
-}
-
 // performs a search and returns the best move
 AI.prototype.getBest = function() {
   var bot = new Bot(this.grid);
   return { move: bot.getBestMove() };
-
-  if (this.canGoUp()) {
-    return { move: '0'};
-  } 
-  if (this.canGoLeft()) {
-    return { move: '3'};
-  } 
-  return { move: '1'};
 }
 
 AI.prototype.translate = function(move) {
